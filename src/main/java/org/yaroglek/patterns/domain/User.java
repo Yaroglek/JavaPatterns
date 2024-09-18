65ff8d97c711913a4f75236b592c85d7959d4e0b
Yaroglek <jkl7589@gmail.com>
2024-09-18 23:53:19 +0500

    Задание 5. Builder Класс User реализует паттерн Builder.

--- src/main/java/org/yaroglek/patterns/domain/User.java
+++ src/main/java/org/yaroglek/patterns/domain/User.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.yaroglek.patterns.domain.enums.UserRole;
 
+/**
+ * Паттерн Builder.
+ */
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
@@ -24,4 +27,48 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+        private Long id;
+        private String username;
+        private String email;
+        private String additionalInfo;
+        private UserRole role;
+
+        UserBuilder() {
+        }
+
+        public UserBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder additionalInfo(String additionalInfo) {
+            this.additionalInfo = additionalInfo;
+            return this;
+        }
+
+        public UserBuilder role(UserRole role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(this.id, this.username, this.email, this.additionalInfo, this.role);
+        }
+    }
 }
